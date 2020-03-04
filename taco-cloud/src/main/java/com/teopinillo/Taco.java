7b81402e766de554ce845fdd55dff0a7e37d3aa9
teopinillo <teopinillo@desktop>
2020-03-04 18:57:18 -0500

    Adding ID to taco class

--- taco-cloud/src/main/java/com/teopinillo/Taco.java
+++ taco-cloud/src/main/java/com/teopinillo/Taco.java
@@ -1,5 +1,6 @@
 package com.teopinillo;
 
+import java.util.Date;
 import java.util.List;
 import lombok.Data;
 import javax.validation.constraints.NotNull;
@@ -7,6 +8,9 @@ import javax.validation.constraints.Size;
 
 @Data
 public class Taco {
+	
+	private Long id;
+	private Date createdAt;
 	
 @NotNull
 @Size (min=5, message="Name must be at least 5 characters long")
