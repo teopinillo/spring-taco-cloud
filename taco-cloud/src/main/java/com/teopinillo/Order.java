a9e3c3a4e1db4f8c2062f54b2012e6f2d075d3bc
teopinillo <teopinillo@desktop>
2020-03-04 18:13:45 -0500

    Declare rules validation onTaco and Order Class.

--- taco-cloud/src/main/java/com/teopinillo/Order.java
+++ taco-cloud/src/main/java/com/teopinillo/Order.java
@@ -1,16 +1,37 @@
 package com.teopinillo;
 
+import javax.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class Order {
 
+	@NotBlank (message="Name is requiered")
 	private String name;
+	
+	@NotBlank (message="Street is requiered")
 	private String street;
+	
+	@NotBlank (message="City is requiered")
 	private String city;
+	
+	@NotBlank (message="State is requiered")
 	private String state;
+	
+	@NotBlank (message="Zip code is requiered")
 	private String zip;
+	
+	@CreditCardNumber (message="Not a valid credit card number")
 	private String ccNumber;
+	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+			message="Must be formatted MM/YY")
 	private String ccExpiration;
+	
+	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
+	
 }
--- taco-cloud/src/main/java/com/teopinillo/Taco.java
+++ taco-cloud/src/main/java/com/teopinillo/Taco.java
@@ -2,9 +2,14 @@ package com.teopinillo;
 
 import java.util.List;
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class Taco {
+	
+@NotNull
+@Size (min=5, message="Name must be at least 5 characters long")
 private String name;
 //Field error in object 'taco' on field 'ingredients': rejected value [FLTO,CARN,CHED,TMTO,SRCR]; 
 //codes [typeMismatch.taco.ingredients,typeMismatch.ingredients,typeMismatch.java.util.List,typeMismatch]; 
@@ -17,5 +22,6 @@ private String name;
 //for property 'ingredients[0]': no matching editors or conversion strategy found]]
 //
 //private List<Ingredient> ingredients;
+@Size(min=1, message="You must choose at least 1 ingredient")
 private List<String> ingredients;
 }
